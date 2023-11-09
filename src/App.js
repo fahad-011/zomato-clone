5a7d8170aa5b0ed51fb7fa6e7af3ab63321268a4
Mohammad Fahad Rahman <kyroc102@gmail.com>
2023-11-09 16:20:41 +0000

    added card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
 import Logo from './images/heroSectionLogo.png';
+import item1 from './images/item1.png';
+import item2 from './images/item2.png';
+import item3 from './images/item3.png';
+import item4 from './images/item4.png';
 
 function App() {
   return (
@@ -57,8 +61,83 @@ function App() {
           />
         </div>
       </div>
-
       </section>
+
+       <section class="container we__offer">
+      <div class="we__offer_card">
+        <img src={item1} alt="item 1 offer card" />
+        <div class="we__offer_content">
+          <h2>Order Online</h2>
+          <p>Stay home and order to your doorstep</p>
+        </div>
+      </div>
+
+      <div class="we__offer_card">
+        <img src={item2} alt="item 2 offer card" />
+        <div class="we__offer_content">
+          <h2>Dining</h2>
+          <p>View the citys favourite dining venue</p>
+        </div>
+      </div>
+
+      <div class="we__offer_card">
+        <img src={item3} alt="item 3 offer card" />
+        <div class="we__offer_content">
+          <h2>Nightlife and clubs</h2>
+          <p>Explore the citys top nightlife outlets</p>
+        </div>
+      </div>
+
+      <div class="we__offer_card">
+        <img src={item4} alt="item 4 offer card" />
+        <div class="we__offer_content">
+          <h2>Order Online</h2>
+          <p>Stay home and order to your doorstep</p>
+        </div>
+      </div>
+    </section>
+
+     <section class="container collections">
+      <h1>Collection</h1>
+      <div>
+        <span>Explore curated lists of top restaurants, cafes, pubs in Hospet-Hampi based on trends</span>
+        <span>All collections in Hospet-Hampi</span>
+      </div>
+      <div class="collections_card_container">
+        <div class="card card1">
+          <div class="overlay"></div>
+          <div class="content">
+            <h4>Live Cricket Screening</h4>
+            <span>56 Places</span>
+          </div>
+        </div>
+
+         <div class="card card2">
+          <div class="overlay"></div>
+          <div class="content">
+            <h4>Malnad Special</h4>
+            <span>12 Places</span>
+          </div>
+        </div>
+
+         <div class="card card3">
+          <div class="overlay"></div>
+          <div class="content">
+            <h4>Newly opened Restaurants</h4>
+            <span>36 Places</span>
+          </div>
+        </div>
+
+         <div class="card card4">
+          <div class="overlay"></div>
+          <div class="content">
+            <h4>Trending Pubs Near Me</h4>
+            <span>16 Places</span>
+          </div>
+        </div>
+
+      </div>
+    </section>      
     
     </div>
   );
